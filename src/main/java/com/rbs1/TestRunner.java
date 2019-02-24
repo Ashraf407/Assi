ccc29dfa15bf145156b7b4f6b6d58552d4ec1c6e
ASHRAF <shaik.ashrafali407@gmail.com>
2019-02-24 19:14:47 +0530

    firest commit

--- src/main/java/com/rbs1/TestRunner.java
+++ src/main/java/com/rbs1/TestRunner.java
@@ -79,17 +79,4 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 	}
 
 
-	@AfterClass
-	public static void teardown16() {
-
-		Reporter.loadXMLConfig(new File("C:\\Users\\ASHRAF\\Desktop\\Selenium\\ExtCucuExamples\\extentConfig.xml"));
-		Reporter.setSystemInfo("user", System.getProperty("user.name"));
-		Reporter.setSystemInfo("os", "Mac OSX");
-		Reporter.setTestRunnerOutput("Sample test runner output message");
-		Reporter.setSystemInfo("os", "Mac OSX");
-		Reporter.setTestRunnerOutput("Sample test runner output message");
-
-
 	}
-
-}
